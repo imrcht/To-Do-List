148f179c3ac6b8f12ffd0727897162ad715a817d
imrcht <rachchooit@gamil.com>
2021-07-20 22:19:16 +0530

    some changes

--- app.js
+++ app.js
@@ -177,6 +177,10 @@ app.post("/clear", (req, res) =>{
 
 
 
-app.listen(7000, () => {
+app.listen(process.env.PORT || 7000, () => {
+    if(process.env.PORT) {
+        console.log(`lsitening to port ${process.env.PORT}`);
+    } else {
     console.log("Listening to port 7000.");
+    }
 })
